a33b35cba34f807f371c8c4ef59c4743366a7e80
FailZorD <alexfred@live.com>
2013-06-14 17:30:51 +0300

    Another Linux Problem
    Title says it all!

--- panel/promo.php
+++ panel/promo.php
@@ -8,7 +8,7 @@
 		
 		<div class="bnet-offer-bg">
 			<a href="search.php" target="_blank" id="vote" class="bnet-offer-image">
-				<img src="wow/static/images/sidebar/Share/armory.png" width="300" height="250" alt=""/>
+				<img src="wow/static/images/sidebar/share/armory.png" width="300" height="250" alt=""/>
 			</a>
 		</div>
 	</div>
