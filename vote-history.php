49e37672ef6c8a565b250b2dad4d899a350e9522
FailZorD <alex_pap_2010@live.com>
2013-09-04 18:46:45 +0300

    Web/Fixes: Some small Fix and re-adding some pictures!
    Signed-off-by: FailZorD <alex_pap_2010@live.com>

--- vote-history.php
+++ vote-history.php
@@ -15,8 +15,6 @@ if (!isset($_SESSION['username'])) {
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 <title>Vote History - <?php echo $website['title']; ?></title>
-<meta content="false" http-equiv="imagetoolbar" />
-<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
 <link rel="shortcut icon" href="wow/static/local-common/images/favicons/wow.png" type="image/x-icon" />
 <link rel="stylesheet" media="all" href="wow/static/local-common/css/common.css" />
 <link rel="stylesheet" media="all" href="wow/static/css/bnet.css" />
@@ -24,6 +22,7 @@ if (!isset($_SESSION['username'])) {
 <link rel="stylesheet" media="all" href="wow/static/css/management/dashboard.css" />
 <link rel="stylesheet" media="all" href="wow/static/css/management/services.css" />
 <link rel="stylesheet" media="all" href="wow/static/css/management/wow/raf.css" />
+<link rel="stylesheet" media="all" href="wow/static/css/management/order-history.css" />
 <script src="wow/static/local-common/js/third-party/jquery-1.4.4-p1.min.js"></script>
 <script src="wow/static/local-common/js/core.js?v22"></script>
 <script src="wow/static/local-common/js/tooltip.js?v22"></script>
@@ -33,34 +32,6 @@ try { document.execCommand('BackgroundImageCache', false, true) } catch(e) {}
 //]]>
 </script>
 <![endif]-->
-<script type="text/javascript">
-//<![CDATA[
-Core.staticUrl = '/account';
-Core.sharedStaticUrl= 'wow/static/local-common';
-Core.baseUrl = '/account';
-Core.projectUrl = '/account';
-Core.cdnUrl = 'http://eu.media.blizzard.com';
-Core.supportUrl = 'http://eu.battle.net/support/';
-Core.secureSupportUrl= 'https://eu.battle.net/support/';
-Core.project = 'bam';
-Core.locale = 'en-us';
-Core.language = 'en';
-Core.buildRegion = 'eu';
-Core.region = 'eu';
-Core.shortDateFormat= 'MM/dd/yyyy';
-Core.dateTimeFormat = 'MM/dd/yyyy hh:mm a';
-Core.loggedIn = true;
-Flash.videoPlayer = 'http://eu.media.blizzard.com/global-video-player/themes/bam/video-player.swf';
-Flash.videoBase = 'http://eu.media.blizzard.com/bam/media/videos';
-Flash.ratingImage = 'http://eu.media.blizzard.com/global-video-player/ratings/bam/rating-en-us.jpg';
-Flash.expressInstall= 'http://eu.media.blizzard.com/global-video-player/expressInstall.swf';
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-544112-16']);
-_gaq.push(['_setDomainName', '.blizzard.net']);
-_gaq.push(['_trackPageview']);
-_gaq.push(['_trackPageLoadTime']);
-//]]>
-</script>
 </head>
 <body class="en-us logged-in">
         <div id="layout-top">
@@ -147,47 +118,6 @@ _gaq.push(['_trackPageLoadTime']);
         echo "</table><br />";
    } else echo '<b>'.$Vote['Vote9'].'</b>';
 ?>
-<script type="text/javascript">
-//<![CDATA[
-$(function() {
-var times = new DateTime(false);
-$('#region-dropdown2').dropdown({
-callback: function(dropdown, selected) {
-var test = $("#view-dropdown2").find("select option:selected").val();
-location.href = 'orders.html?rId='+ selected;
-},
-updateText: false
-});
-$('#view-dropdown2').dropdown({
-callback: function(dropdown, selected) {
-if(selected == "1"){selected = '';}
-switch (selected) {
-case "2":
-selected = "chargeback";
-break;
-}
-orderTable.filter('class', 'class', selected);
-},
-updateText: true
-});
-var orderTable = new Table('#order-history');
-$('#order-history tr').hover(
-function() {
-var activeRow = $(this);
-activeRow.addClass('row-hover');
-activeRow.nextUntil('.parent-row').addClass('row-hover');
-if (activeRow.hasClass('child-row')) {
-activeRow.prevUntil('.parent-row').addClass('row-hover');
-activeRow.prevAll('.parent-row').eq(0).addClass('row-hover');
-}
-},
-function() {
-$('#order-history tr').removeClass('row-hover');
-}
-);
-});
-//]]>
-</script>
 </div>
 </div>
 </div>
@@ -195,119 +125,9 @@ $('#order-history tr').removeClass('row-hover');
 <div id="layout-bottom">
 <?php include("functions/footer_man.php"); ?>
 </div>
-<script type="text/javascript">
-//<![CDATA[
-var xsToken = '1719af93-c8ae-4514-b0ba-68fbf28147b5';
-var Msg = {
-support: {
-ticketNew: 'Ticket {0} was created.',
-ticketStatus: 'Ticket {0}’s status changed to {1}.',
-ticketOpen: 'Open',
-ticketAnswered: 'Answered',
-ticketResolved: 'Resolved',
-ticketCanceled: 'Canceled',
-ticketArchived: 'Archived',
-ticketInfo: 'Need Info',
-ticketAll: 'View All Tickets'
-},
-cms: {
-requestError: 'Your request cannot be completed.',
-ignoreNot: 'Not ignoring this user',
-ignoreAlready: 'Already ignoring this user',
-stickyRequested: 'Sticky requested',
-stickyHasBeenRequested: 'You have already sent a sticky request for this topic.',
-postAdded: 'Post added to tracker',
-postRemoved: 'Post removed from tracker',
-userAdded: 'User added to tracker',
-userRemoved: 'User removed from tracker',
-validationError: 'A required field is incomplete',
-characterExceed: 'The post body exceeds XXXXXX characters.',
-searchFor: "Search for",
-searchTags: "Articles tagged:",
-characterAjaxError: "You may have become logged out. Please refresh the page and try again.",
-ilvl: "Level {0}",
-shortQuery: "Search requests must be at least three characters long."
-},
-bml: {
-bold: 'Bold',
-italics: 'Italics',
-underline: 'Underline',
-list: 'Unordered List',
-listItem: 'List Item',
-quote: 'Quote',
-quoteBy: 'Posted by {0}',
-unformat: 'Remove Formating',
-cleanup: 'Fix Linebreaks',
-code: 'Code Blocks',
-item: 'WoW Item',
-itemPrompt: 'Item ID:',
-url: 'URL',
-urlPrompt: 'URL Address:'
-},
-ui: {
-submit: 'Submit',
-cancel: 'Cancel',
-reset: 'Reset',
-viewInGallery: 'View in gallery',
-loading: 'Loading…',
-unexpectedError: 'An error has occurred',
-fansiteFind: 'Find this on…',
-fansiteFindType: 'Find {0} on…',
-fansiteNone: 'No fansites available.'
-},
-grammar: {
-colon: '{0}:',
-first: 'First',
-last: 'Last'
-},
-fansite: {
-achievement: 'achievement',
-character: 'character',
-faction: 'faction',
-'class': 'class',
-object: 'object',
-talentcalc: 'talents',
-skill: 'profession',
-quest: 'quest',
-spell: 'spell',
-event: 'event',
-title: 'title',
-arena: 'arena team',
-guild: 'guild',
-zone: 'zone',
-item: 'item',
-race: 'race',
-npc: 'NPC',
-pet: 'pet'
-},
-search: {
-kb: 'Support',
-post: 'Forums',
-article: 'Blog Articles',
-static: 'General Content',
-wowcharacter: 'Characters',
-wowitem: 'Items',
-wowguild: 'Guilds',
-wowarenateam: 'Arena Teams',
-other: 'Other'
-}
-};
-//]]>
-</script>
 <script src="wow/static/js/bam.js"></script>
 <script src="wow/static/local-common/js/tooltip.js"></script>
 <script src="wow/static/local-common/js/menu.js"></script>
-<script type="text/javascript">
-$(function() {
-Menu.initialize();
-Menu.config.colWidth = 190;
-Locale.dataPath = 'data/i18n.frag.xml';
-});
-</script>
-<!--[if lt IE 8]>
-<script type="text/javascript" src="wow/static/local-common/js/third-party/jquery.pngFix.pack.js"></script>
-<script type="text/javascript">$('.png-fix').pngFix();</script>
-<![endif]-->
 <script src="wow/static/local-common/js/dropdown.js"></script>
 <script src="wow/static/local-common/js/table.js"></script>
 <script type="text/javascript">
@@ -319,29 +139,6 @@ if (typeof Login !== 'undefined') {
 Login.embeddedUrl = '<?php echo $website['root'];?>loginframe.php';
 }
 });
-//]]>
-</script>
-<!--[if lt IE 8]> <script type="text/javascript" src="wow/static/local-common/js/third-party/jquery.pngFix.pack.js"></script>
-<script type="text/javascript">
-//<![CDATA[
-$('.png-fix').pngFix(); //]]>
-</script>
-<![endif]-->
-<script type="text/javascript">
-//<![CDATA[
-(function() {
-var ga = document.createElement('script');
-var src = "https://ssl.google-analytics.com/ga.js";
-if ('http:' == document.location.protocol) {
-src = "http://www.google-analytics.com/ga.js";
-}
-ga.type = 'text/javascript';
-ga.setAttribute('async', 'true');
-ga.src = src;
-var s = document.getElementsByTagName('script');
-s = s[s.length-1];
-s.parentNode.insertBefore(ga, s.nextSibling);
-})();
 //]]>
 </script>
 </body>
