6f3e8748017bcd6c76680d353fd387b12c4d9b58
FailZorD <alex_pap_2010@live.com>
2013-11-13 11:57:07 +0200

    Web/fix: Another Small fix for Windows. Might work for Linux. Needs to be checked.
    Signed-off-by: FailZorD <alex_pap_2010@live.com>

--- forum/functions.d/GetForumTheme.php
+++ forum/functions.d/GetForumTheme.php
@@ -8,11 +8,11 @@
 	while($row=mysqli_fetch_array($result))
 	{
 	$CSS_LINK = $row['css_link'];
-echo '<link rel="stylesheet" href="../../wow/static/Themes/'.$CSS_LINK.'/css/wow.css?v4" />';
-echo '<link rel="stylesheet" href="../../wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/forums.css?v15" />';
-echo '<link rel="stylesheet" href="../../wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/cms-common.css?v15" />';
-echo '<link rel="stylesheet" href="../../wow/static/Themes/'.$CSS_LINK.'/css/cms.css?v4" />';
-echo '<link rel="stylesheet" href="../../wow/static/Themes/'.$CSS_LINK.'/local-common/css/common.css?v15" />';
+echo '<link rel="stylesheet" href="../wow/static/Themes/'.$CSS_LINK.'/css/wow.css?v4" />';
+echo '<link rel="stylesheet" href="../wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/forums.css?v15" />';
+echo '<link rel="stylesheet" href="../wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/cms-common.css?v15" />';
+echo '<link rel="stylesheet" href="../wow/static/Themes/'.$CSS_LINK.'/css/cms.css?v4" />';
+echo '<link rel="stylesheet" href="../wow/static/Themes/'.$CSS_LINK.'/local-common/css/common.css?v15" />';
 
 }
 }
