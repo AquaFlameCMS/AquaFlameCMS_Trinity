fc5d7902138adcd6140886d80107cc2db8878764
FailZorD <alex_pap_2010@live.com>
2013-11-18 11:49:59 +0200

    Web/Revert: Missed that one.

--- configs.php
+++ configs.php
@@ -76,7 +76,7 @@ $serveruser    = "root";
 $serverpass    = "password";
 $serverport    = "3306";
 define('DBHOST', '127.0.0.1');
-define('DBUSER', 'web46');
+define('DBUSER', 'root');
 define('DBPASS', 'password');
 define('DB', 'website');
 
