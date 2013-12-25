4a3d72a7222bbbdeb602cc9456b3754e718d8ae3
One <luizcortesj@gmail.com>
2013-12-25 14:49:42 -0600

    Web/Fix: style error pages

--- errors/404.php
+++ errors/404.php
@@ -2,30 +2,23 @@
 require_once("../configs.php");
 $page_cat = "home";
 ?>
+<html lang="en-gb">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+<html>
 <head>
 	<title><?php echo $website['title']; ?></title>
 	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
 	<meta name="description" content="<?php echo $website['description']; ?>">
 	<meta name="keywords" content="<?php echo $website['keywords']; ?>">
-	<link rel="shortcut icon" href="../wow/static/local-common/images/favicons/wow.png" type="image/x-icon" />
-	<link rel="stylesheet" type="text/css" media="all" href="../wow/static/local-common/css/common.css?v15" />
-	<!--[if IE]><link rel="stylesheet" type="text/css" media="all" href="../wow/static/local-common/css/common-ie.css?v15" /><![endif]-->
-	<!--[if IE 6]><link rel="stylesheet" type="text/css" media="all" href="../wow/static/local-common/css/common-ie6.css?v15" /><![endif]-->
-	<!--[if IE 7]><link rel="stylesheet" type="text/css" media="all" href="../wow/static/local-common/css/common-ie7.css?v15" /><![endif]-->
-	<link title="World of Warcraft - News" href="../wow/en/feed/news" type="application/atom+xml" rel="alternate"/>
-	<link rel="stylesheet" type="text/css" media="all" href="../wow/static/css/wow.css?v4" />
-	<link rel="stylesheet" type="text/css" media="all" href="../wow/static/local-common/css/cms/homepage.css?v15" />
-	<link rel="stylesheet" type="text/css" media="all" href="../wow/static/local-common/css/cms/blog.css?v15" />
-	<link rel="stylesheet" type="text/css" media="all" href="../wow/static/local-common/css/cms/cms-common.css?v15" />
-	<link rel="stylesheet" type="text/css" media="all" href="../wow/static/css/cms.css?v4" />
-  <link rel="stylesheet" type="text/css" media="all" href="../wow/static/css/wow.css?v23" />
-	<!--[if IE 6]><link rel="stylesheet" type="text/css" media="all" href="../wow/static/css/cms-ie6.css?v4" /><![endif]-->
-	<!--[if IE]><link rel="stylesheet" type="text/css" media="all" href="../wow/static/css/wow-ie.css?v4" /><![endif]-->
-	<!--[if IE 6]><link rel="stylesheet" type="text/css" media="all" href="../wow/static/css/wow-ie6.css?v4" /><![endif]-->
-	<!--[if IE 7]><link rel="stylesheet" type="text/css" media="all" href="../wow/static/css/wow-ie7.css?v4" /><![endif]-->
-	<script type="text/javascript" src="../wow/static/local-common/js/third-party/jquery-1.4.4-p1.min.js"></script>
-	<script type="text/javascript" src="../wow/static/local-common/js/core.js?v15"></script>
-	<script type="text/javascript" src="../wow/static/local-common/js/tooltip.js?v15"></script>
+	<link rel="shortcut icon" href="<?php echo $website['root']; ?>wow/static/local-common/images/favicons/wow.png" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo $website['root']; ?>wow/static/local-common/css/common.css?v15" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo $website['root']; ?>wow/static/css/wow.css?v4" />
+	<script type="text/javascript" src="<?php echo $website['root']; ?>wow/static/local-common/js/third-party/jquery-1.4.4-p1.min.js"></script>
+	<script type="text/javascript" src="<?php echo $website['root']; ?>wow/static/local-common/js/core.js?v15"></script>
+	<script type="text/javascript" src="<?php echo $website['root']; ?>wow/static/local-common/js/tooltip.js?v15"></script>
 	<script type="text/javascript" src="http://static.wowhead.com/widgets/power.js"></script>
 	<!--[if IE 6]> <script type="text/javascript">
 	//<![CDATA[
