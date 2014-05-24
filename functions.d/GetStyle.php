6dcb1dcd74dd61b5666d94e7efd33519391caea6
One <projectonecore@gmail.com>
2014-05-24 16:29:36 -0600

    Web/Fix: last commits and update account...

--- functions.d/GetStyle.php
+++ functions.d/GetStyle.php
@@ -6,19 +6,10 @@
 	while($row=mysqli_fetch_array($result))
 	{
 	$CSS_LINK = $row['css_link'];
-	echo '<link rel="stylesheet" type="text/css" media="all" href="wow/static/Themes/'.$CSS_LINK.'/local-common/css/common.css?v15">';
-	echo '<link rel="stylesheet"  href="wow/static/Themes/'.$CSS_LINK.'/css/wow.css?v4">';
-	echo '<link rel="stylesheet"  href="wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/homepage.css?v15">';
-	echo '<link rel="stylesheet"  href="wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/blog.css?v15">';
-	echo '<link rel="stylesheet"  href="wow/static/Themes/'.$CSS_LINK.'/css/status.css?v1">';
-	echo '<link rel="stylesheet"  href="wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/cms-common.css?v15">';
-	echo '<link rel="stylesheet"  href="wow/static/Themes/'.$CSS_LINK.'/css/cms.css?v4">';
-	echo '<link rel="stylesheet" media="all" href="wow/static/Themes/'.$CSS_LINK.'/local-common/css/common.css?v46" />';
-	echo '<link rel="stylesheet" media="all" href="wow/static/Themes/'.$CSS_LINK.'/css/wow.css?v34" />';
-	echo '<link rel="stylesheet" media="all" href="wow/static/Themes/'.$CSS_LINK.'/css/community/community.css?v34" />';
-	echo '<link rel="stylesheet" media="all" href="wow/static/Themes/'.$CSS_LINK.'/css/cms.css?v34" />';
-	echo '<link rel="stylesheet" media="all" href="wow/static/Themes/'.$CSS_LINK.'/css/locale/en-gb.css?v34" />';
-	echo '<link rel="stylesheet" href="wow/static/Themes/'.$CSS_LINK.'/css/lightbox.css?v7" />';
-	echo '<link rel="stylesheet" href="wow/static/Themes/'.$CSS_LINK.'/local-common/css/cms/comments.css?v15" />';
+	echo '<link rel="stylesheet" type="text/css" media="all" href="wow/static/Themes/'.$CSS_LINK.'/local-common/css/common-game-site.css" />';
+	echo '<link rel="stylesheet" type="text/css" media="all" href="wow/static/Themes/'.$CSS_LINK.'/css/wow.css" />';
+	echo '<link rel="stylesheet" type="text/css" media="all" href="wow/static/Themes/'.$CSS_LINK.'/css/lightbox.css" />';
+	echo '<link rel="stylesheet" type="text/css" media="all" href="wow/static/Themes/'.$CSS_LINK.'/cms-overlay/css/build/cms.min.css" />';
+	echo '<link rel="stylesheet" type="text/css" media="all" href="wow/static/Themes/'.$CSS_LINK.'/css/cms.css" />';
 }
 }
--- functions/header_account.php
+++ functions/header_account.php
@@ -1,3 +1,12 @@
+<div id="search-bar">
+	<form action="<?php echo $website['root']; ?>search.php" method="get" id="search-form">
+		<div>
+			<input type="text" name="q" id="search-field" value="Search <?php echo $website['title']; ?>" maxlength="35" alt="Search <?php echo $website['title']; ?>" tabindex="50" accesskey="q" class="" />
+			<input type="submit" id="search-button" value="" title="Search <?php echo $website['title']; ?>" tabindex="50" />
+		</div>
+	</form>
+</div>
+<h1 id="logo"><a href="<?php echo $website['root']; ?>" tabindex="50" accesskey="h">Battle.net</a></h1>
 <div id="navigation">
 <div id="page-menu" class="large">
 <h2><a href="<?php echo $website['root']; ?>account/"> <?php echo $ha['ha26']; ?>
@@ -60,12 +69,9 @@
 <span></span>
 </li>
 
-<li <?php if($page_cat=='transaction') echo'class="active"';?>>
-<a href="<?php echo $website['root']; ?>" class="border-3"><?php echo $ha['ha25']; ?></a>
-<span></span>
-</li>
 
-<li class="account-balance" id="accountBalanceCenter" data-tooltip-options="{&quot;location&quot;: &quot;mouse&quot;}">
+
+<li class="account-balance account-balance-usd" id="accountBalanceCenter" data-tooltip-options="{&quot;location&quot;: &quot;mouse&quot;}">
 <a href="#" class="border-3 menu-arrow title" onclick="openAccountDropdown(this, 'accountBalance'); return false;">
 <span class="sub-title">Credits:</span><br />
 <?php
@@ -74,20 +80,93 @@ while($get = mysql_fetch_array($points))
 {
 ?>
 <span class="balance" id="primary-balance"><?php echo $get["credits"];?></span>
-</a>
 <?php } ?>
+</a>
 <div class="flyout-menu" id="accountBalance-menu">
 <ul>
 <li class=" first nonBeta"><a href="<?php echo $website['root']; ?>account/sms.php">Add Funds</a></li>
-<!--<li class=""><a href="#">Balance History</a></li>
-<li class="line nonBeta"><a href="#" onclick="$('#account-balance-dialog').dialog('open'); return false;">Other Currencies...</a></li>
-<li class="nonBeta"><a href="#">Balance Help</a></li>-->
+<li class=" "><a href="#">Add Pre-Paid Card</a></li>
+<li class=" line "><a href="#">Balance History</a></li>
+<li class="line"><a href="#" onclick="$('#account-balance-dialog').dialog('open'); return false;">Other Currencies</a></li>
+<li class="line"><a href="#">Balance Help</a></li>
+<li id="refreshBalance"><a href="#" onclick="accountBalance.refreshBalance(); return false;">Refresh Balance</a></li>
+<!--<li class="refreshing" id="refreshingBalance"><a href="#" onclick="return false;"><img src="/account/static/images/icons/loader.gif" alt="" height="11" width="16" />Refreshing…</a></li>-->
 </ul>
 </div>
 </li>
 </ul>
 <span class="clear"><!-- --></span>
 </div>
+<script type="text/javascript">
+//<![CDATA[
+$(function() {
+accountBalance.accountBalanceCurrency = "USD";
+accountBalance.currencyMap = {
+'USD' : {
+'format': '{0} USD',
+'groupSize': 3,
+'delimiter': '.',
+'point': ','
+},
+'BRL' : {
+'format': '{0} BRL',
+'groupSize': 3,
+'delimiter': '.',
+'point': ','
+},
+'ARS' : {
+'format': '{0} ARS',
+'groupSize': 3,
+'delimiter': '.',
+'point': ','
+},
+'CLP' : {
+'format': '{0} CLP',
+'groupSize': 3,
+'delimiter': '.',
+'point': ','
+},
+'MXN' : {
+'format': '{0} MXN',
+'groupSize': 3,
+'delimiter': '.',
+'point': ','
+},
+'AUD' : {
+'format': '{0} AUD',
+'groupSize': 3,
+'delimiter': '.',
+'point': ','
+},
+'BET' : {
+'format': 'BZ${0}',
+'groupSize': 3,
+'delimiter': '.',
+'point': ','
+}
+};
+accountBalance.initialize();
+$('.account-balance-dialog').dialog({
+autoOpen: false,
+modal: true,
+position: "center",
+resizeable: false,
+closeText: "Cerrar",
+buttons: {
+'Ok': function() {
+$(this).dialog("close");
+}
+},
+open: function() {
+$(".ui-dialog-buttonpane").find("button").addClass("button1").find(":first").addClass("first");
+if(Core.browser=="ie6" || Core.browser=="ie7" || Core.browser=="ie8"){
+$(".ui-widget-overlay").css("opacity", 0.8);
+}
+}
+});
+});
+//]]>
+</script>
 <span class="clear"></span>
 </div>
 </div>
