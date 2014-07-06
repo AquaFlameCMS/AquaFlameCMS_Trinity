153d6004ce529b9ccc862b436194348b04642872
One <projectonecore@gmail.com>
2014-07-06 13:54:06 -0600

    Web: implementing random images..

--- panel/promo.php
+++ panel/promo.php
@@ -1,10 +1,48 @@
+<?php
+/**
+ * Random images
+ */
+$imagenes[0]='0.jpg';
+$imagenes[1]='1.jpg';
+$imagenes[2]='2.jpg';
+$imagenes[3]='3.jpg';
+
+/**
+ * Description Images
+ */
+$descripcion[0]='Random Image 0';
+$descripcion[1]='Random Image 1';
+$descripcion[2]='Random Image 2';
+$descripcion[3]='Random Image 3';
+
+/**
+ * URL of random images
+ */
+$href[0]='game/';
+$href[1]='search.php';
+$href[2]='account/';
+$href[3]='account/';
+
+/* Here it is important to give it the number of images starting from 0 */
+$i=rand(0,4);
+?>
 <div id="sidebar-marketing" class="sidebar-module">
-<div class="bnet-offer">
-<!--  -->
-<div class="bnet-offer-bg">
-<a href="search.php" target="_blank" id="ad10069924" class="bnet-offer-image">
-<img src="<?php echo $website['root']; ?>wow/static/images/sidebar/Share/armory.png" width="300" height="250" alt=""/>
-</a>
-</div>
-</div>
+	<div class="bnet-offer">
+		<!--  -->
+		<div class="bnet-offer-bg">
+			<a href="<?php echo $website['root']; ?><?php echo $href[$i];?>" target="_blank" id="ad10069924" class="bnet-offer-image">
+			<?php
+				$con=mysqli_connect(DBHOST,DBUSER,DBPASS,DB);
+				$query= "SELECT * FROM themes WHERE active=1";
+				$result=mysqli_query($con,$query);
+				while($row=mysqli_fetch_array($result))
+				{
+				$CSS_LINK = $row['css_link'];
+				/* Show random picture */
+				echo '<img src="'.$website['root'].'wow/static/Themes/'.$CSS_LINK.'/images/panel/'.$imagenes[$i].'" width="300" height="250" alt="'.$descripcion[$i].'"/>'; 
+				}
+			?>
+			</a>
+		</div>
+	</div>
 </div>
