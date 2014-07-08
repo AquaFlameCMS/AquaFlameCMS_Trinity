1511f74068193383fd96d35af77957dc01043758
One <projectonecore@gmail.com>
2014-07-08 17:47:15 -0600

    Web/fix: fixed last commits, still i can't make this change u.u

--- register.php
+++ register.php
@@ -76,7 +76,7 @@ _gaq.push(['_trackPageLoadTime']);
 function valid_email($email) //Small function to validate the email
 {
     $result = TRUE;
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)) {
         $result = FALSE;
     }
     return $result;
