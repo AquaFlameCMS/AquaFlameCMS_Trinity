a84c2fb0bbd721481d681b133d01267118e7b3f3
Alexandros Papadopoulos <alex_pap_2010@live.com>
2015-04-01 05:21:41 +0300

    [Pre-Final] AquaFlame is OLD
    Aquaflame is being Marked as OLD and UNUSABLE.
    It should not be used because the developers will not support it. For
    more information read the README file.

--- admin/footer.php
+++
@@ -1,4 +0,0 @@
-<div id="foot">
-          <p> All rights reserved.  |  Powered by: <a href="" target="_blank"><font color="#15509E">AquaFlame CMS</font></a></p>
-		  <center><a class="powered" href="http://www.strawberry-pr0jcts.com/" ></a></center>
-        </div>
